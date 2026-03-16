cd60ce1e67f7f4c267f6e61824f70d8ebf1bd637
luisfelipec95 <luisfelipec95@gmail.com>
2026-03-16 17:16:23 -0500

    chore: fix import django urls

--- secure_cloudfront_video/urls.py
+++ secure_cloudfront_video/urls.py
@@ -13,13 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.urls import re_path
 
 from secure_cloudfront_video.views import secure_cloudfront_video
 
 urlpatterns = [
-    url(
-        r'',
+    re_path(
+        r'^$',
         secure_cloudfront_video,
         name='secure_cloudfront_video',
     ),
