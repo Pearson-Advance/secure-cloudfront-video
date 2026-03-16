7cb6d33302f46d68ac4d3d0506bc50a26724ef95
luisfelipec95 <luisfelipec95@gmail.com>
2026-03-16 15:50:09 -0500

    chore: test

--- secure_cloudfront_video/urls.py
+++ secure_cloudfront_video/urls.py
@@ -13,13 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.urls import re_path
+from django.conf.urls import url
 
 from secure_cloudfront_video.views import secure_cloudfront_video
 
 urlpatterns = [
-    re_path(
-        r'^$',
+    url(
+        r'',
         secure_cloudfront_video,
         name='secure_cloudfront_video',
     ),
